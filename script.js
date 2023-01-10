ec4a883c770dcfb0792887239bd2c06b0e452610
Harrison Boatman <hrry.boatman2@gmail.com>
2023-01-09 21:54:05 -0600

    removed comments from challenge requirement

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 var btn = $('.btn');
 var txt = $('.description');
 var timeDisplayEl = $('#currentDay');
@@ -36,24 +34,7 @@ $(document).ready(function () {
       }
     })
   };
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
 function displayTime() {
   var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
@@ -72,7 +53,7 @@ $('#hour7 .description').val(localStorage.getItem("hour7"));
 $('#hour8 .description').val(localStorage.getItem("hour8"));
 $('#hour9 .description').val(localStorage.getItem("hour9"));
 $('#hour10 .description').val(localStorage.getItem("hour10"));
-$('#hour11.description').val(localStorage.getItem("hour11"));
+$('#hour11 .description').val(localStorage.getItem("hour11"));
 $('#hour12 .description').val(localStorage.getItem("hour12"));
 $('#hour13 .description').val(localStorage.getItem("hour13"));
 $('#hour14 .description').val(localStorage.getItem("hour14"));
